c80c2194f4d9d9ad6fb3b8475d3383d40ed3ec8e
David Coterillo Gonzalez <coterillogd@unican.es>
2026-06-30 14:58:48 +0200

    fix: revert local change

--- template.net10.api/Business/BusinessConstants.cs
+++ template.net10.api/Business/BusinessConstants.cs
@@ -8,7 +8,7 @@ internal static class BusinessConstants
     /// <summary>
     ///     The display name or namespace identifier for this API project.
     /// </summary>
-    internal const string ApiName = "template.net10.api";
+    internal const string ApiName = "{PROJECT_NAMESPACE}";
 
     // add more constants here as needed
 }
